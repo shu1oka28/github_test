7ee0039f1201522ecc0c41685a0e05cbb5c6e824
shu1oka28 <40012404+shu1oka28@users.noreply.github.com>
2021-08-18 01:15:53 +0900

    fix a bug

--- case/secret_artifact/README.md
+++ case/secret_artifact/README.md
@@ -1,4 +1,12 @@
+secret_artifact
+================
+
+secret.txtをgithubのリポジトリのsecretsに入れ、secretsの内容をGitHub ActionsでArtifactに入れる
+Publicリポジトリで機密情報をActionsに送ることができることがわかった。
+
 ```
+sudo apt install python3-pip
+pip3 install -r requirements.txt
 ./run.sh user_id token
 ```
 user_idはユーザID
--- case/secret_artifact/run.sh
+++ case/secret_artifact/run.sh
@@ -7,7 +7,7 @@ ENCRYPTED=`python3 encrypt.py $PUBLIC_KEY secret_sample.txt`
 BODY="{\"encrypted_value\": \"${ENCRYPTED}\", \"key_id\": ${PUBLIC_KEYID}}"
 curl -u $1:$2 -X PUT -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/secrets/secret_file -d "${BODY}" 
 WORKFLOW_ID=`curl -u $1:$2 -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/workflows|jq '.workflows[]| select(.path == ".github/workflows/case_secret_artifact.yml").id'` 
-curl -u $1:$2 -X POST -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/workflows/$WORKFLOW_ID/dispatches 
+curl -u $1:$2 -X POST -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/workflows/$WORKFLOW_ID/dispatches -d '{"ref":"main"}' 
 sleep 60
 curl -u $1:$2 -X DELETE -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/secrets/secret_file
 
