6247c59ea17113abe7def3bd1f087e9e9f896d0c
shu1oka28 <40012404+shu1oka28@users.noreply.github.com>
2021-08-18 19:32:40 +0900

    add

--- case/secret_artifact/run.sh
+++ case/secret_artifact/run.sh
@@ -6,8 +6,25 @@ PUBLIC_KEYID=`echo $KEYS|jq '.key_id'`
 ENCRYPTED=`python3 encrypt.py $PUBLIC_KEY secret_sample.txt`
 BODY="{\"encrypted_value\": \"${ENCRYPTED}\", \"key_id\": ${PUBLIC_KEYID}}"
 curl -u $1:$2 -X PUT -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/secrets/secret_file -d "${BODY}" 
-WORKFLOW_ID=`curl -u $1:$2 -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/workflows|jq '.workflows[]| select(.path == ".github/workflows/case_secret_artifact.yml").id'` 
+WORKFLOWS=`curl -u $1:$2 -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/workflows`
+WORKFLOW_ID=`echo $WORKFLOWS|jq '.workflows[]| select(.path == ".github/workflows/case_secret_artifact.yml").id'` 
+WORKFLOW_NODEID=`echo $WORKFLOWS|jq '.workflows[]| select(.path == ".github/workflows/case_secret_artifact.yml").node_id'` 
 curl -u $1:$2 -X POST -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/workflows/$WORKFLOW_ID/dispatches -d '{"ref":"main"}' 
-sleep 60
+
+flag=true
+while $flag
+do
+sleep 5
+ARTIFACTS=`curl -u $1:$2 -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/artifacts|jq '.total_count'`
+if [ "$ARTIFACTS" -ne 0 ];then
+	ARTIFACTS=`curl -u $1:$2 -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/artifacts|jq '.artifacts[]|select(.name=="result").archive_download_url'`
+	echo $ARTIFACTS
+	if [ -n "$ARTIFACTS" ];then
+		flag=false
+	fi
+fi
+done
+ARTIFACTS=`curl -u $1:$2 -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/artifacts|jq '.artifacts[]|select("name"=="result").archive_download_url[0]'`
+
 curl -u $1:$2 -X DELETE -H "accept: application/vnd.github.v3+json" https://api.github.com/repos/shu1oka28/github_test/actions/secrets/secret_file
 
